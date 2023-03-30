2f5d03526994011e8eea99acc8154861407a32b9
Your Name <you@example.com>
2023-03-30 12:53:01 +0100

    allauth issue

--- carter_music_co/urls.py
+++ carter_music_co/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'carter-music-co'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
